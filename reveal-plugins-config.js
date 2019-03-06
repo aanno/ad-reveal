ec518a1a92736a206c2d4d9bd0abf348340c9a55
Thomas Pasch <thomas.pasch@siteos.de>
2019-03-06 15:01:56 +0100

    added mathjax support

---
+++ reveal-plugins-config.js
@@ -0,0 +1,7 @@
+// reveal-plugins-config.js 
+
+	math: {
+		// mathjax: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js',
+		mathjax: 'node_modules/mathjax/MathJax.js',
+		config: 'TeX-AMS_HTML-full'
+	},
---
+++ reveal-plugins.js
@@ -0,0 +1,3 @@
+// reveal-plugins.js
+{ src: 'node_modules/reveal.js/plugin/math/math.js', async: true },
+{ src: 'node_modules/reveal.js/plugin/multiplex/index.js', async: true },
