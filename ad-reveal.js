f2a68d98b4a57633845749c0ac74280846449f8d
Thomas Pasch <thomas.pasch@siteos.de>
2019-03-06 14:17:46 +0100

    initial

---
+++ ad-reveal.js
@@ -0,0 +1,11 @@
+// https://asciidoctor.org/docs/asciidoctor-revealjs/
+
+// Load asciidoctor.js and asciidoctor-reveal.js
+var asciidoctor = require('asciidoctor.js')();
+var asciidoctorRevealjs = require('asciidoctor-reveal.js');
+asciidoctorRevealjs.register()
+
+// Convert the document 'presentation.adoc' using the reveal.js converter
+var options = {safe: 'safe', backend: 'revealjs'};
+asciidoctor.convertFile('presentation.adoc', options); 
+
