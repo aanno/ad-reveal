da8a0dbe440a2a8213aee0482014624c003e9deb
Thomas Pasch <thomas.pasch@siteos.de>
2019-03-06 15:09:44 +0100

    fixes

--- reveal-plugins.js
+++ reveal-plugins.js
@@ -1,3 +1,5 @@
 // reveal-plugins.js
 { src: 'node_modules/reveal.js/plugin/math/math.js', async: true },
+{ src: 'node_modules/reveal.js/plugin/markdown/marked.js', async: true },
+{ src: 'node_modules/reveal.js/plugin/markdown/markdown.js', async: true },
 { src: 'node_modules/reveal.js/plugin/multiplex/index.js', async: true },
