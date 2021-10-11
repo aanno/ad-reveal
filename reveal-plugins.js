e22ea42e75e727452072077c98b75ffce25d742d
aanno <aanno@users.noreply.github.com>
2021-10-11 17:14:30 +0200

    direct mathjax

--- reveal-plugins.js
+++ reveal-plugins.js
@@ -2,4 +2,5 @@
 { src: 'node_modules/reveal.js/plugin/math/math.js', async: true },
 { src: 'node_modules/reveal.js/plugin/markdown/marked.js', async: true },
 { src: 'node_modules/reveal.js/plugin/markdown/markdown.js', async: true },
-{ src: 'node_modules/reveal.js/plugin/multiplex/index.js', async: true },
+// including results in 'Uncaught ReferenceError: require is not defined' with FF93
+// { src: 'node_modules/reveal.js/plugin/multiplex/index.js', async: true },
