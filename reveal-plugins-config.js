1f861e74c36d7e0d23ee8acd540cabf8da486ab4
aanno <aanno@users.noreply.github.com>
2021-10-11 12:23:29 +0200

    fixed math with MathJax version 2.7.9

--- reveal-plugins-config.js
+++ reveal-plugins-config.js
@@ -1,7 +1,9 @@
 // reveal-plugins-config.js 
 
-	math: {
-		// mathjax: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js',
-		mathjax: 'node_modules/mathjax/MathJax.js',
-		config: 'TeX-AMS_HTML-full'
-	},
+math: {
+    // Attention: Reveal still uses MathJax version 2
+    // https://revealjs.com/math/
+    mathjax: 'https://cdn.jsdelivr.net/gh/mathjax/mathjax@2.7.8/MathJax.js',
+    // mathjax: 'node_modules/mathjax/MathJax.js',
+    config: 'TeX-AMS_HTML-full'
+},
